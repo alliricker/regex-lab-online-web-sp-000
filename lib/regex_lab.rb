3bffa7dfcee7ddbd9e0496860aecb378af268c1f
Allison Ricker <alliricker@gmail.com>
2020-09-17 21:46:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,11 @@ text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-text.scan(/^[a-z]{5}$/i)
+text.scan(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/\w)
 end
 
 def valid_phone_number?(phone)
